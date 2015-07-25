991e47c1602aebf0eb593c85cf7843fd3093d00d
GeorgyPorgy <glorycorp@gmail.com>
2015-07-25 13:38:38 +0300

    #2 - main.c reworked

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
-#define WS
+#include <stdio.h>
+#define LAST 10
+      
+int main()
+{
+    int i, sum = 0;
+     
+    for ( i = 1; i <= LAST; i++ ) {
+      sum += i;
+    } /*-for-*/
+    printf("sum = %d\n", sum);
+
+    return 0;
+}
